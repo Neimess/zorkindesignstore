d4e50e468dad526792fe1383bbf26b82ecb150cc
= <=>
2025-06-23 20:31:29 +0300

    args

--- backend/pkg/args/args.go
+++ backend/pkg/args/args.go
@@ -3,10 +3,6 @@ package args
 import "github.com/alexflint/go-arg"
 
 type Args struct {
-	Up         bool   `arg:"--up" help:"Run database migrations"`
-	Force      int    `arg:"--force"      help:"Force database to specific migration version before running --migrate (0 - skip)"`
-	Down       bool   `arg:"--down" help:"Rollback the last migration step"`
-	DSN        string `arg:"--dsn" help:"DSN to connect DB"`
 	ConfigPath string `arg:"-c,--config"  help:"Path to the configuration file"`
 }
 
